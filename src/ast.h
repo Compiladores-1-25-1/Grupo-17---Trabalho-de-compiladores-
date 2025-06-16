adaa94e5ba198bc3542151bc41f58a0aaa525a1e
Manoel835 <mftneto@gmail.com>
2025-06-16 02:48:03 -0300

    add ast-if

--- src/ast.h
+++ src/ast.h
@@ -7,8 +7,24 @@
 NoAST *criarNoOp(char op, NoAST *esq, NoAST *dir);
 NoAST *criarNoNum(int val);
 NoAST *criarNoId(char *nome, Tipo tipo);
+
+
+NoAST *criarNoIf(NoAST *condicao, NoAST *bloco_then, NoAST *bloco_else);
+NoAST *criarNoWhile(NoAST *condicao, NoAST *bloco_body);
+NoAST *criarNoImprima(NoAST *expressao);
+NoAST *criarNoLeia(char *nome);
+NoAST *criarNoAtribuicao(char *nome, NoAST *expressao);
+NoAST *criarNoDeclaracao(char *nome, Tipo tipo);
+NoAST *criarNoBloco();
+void adicionarComandoBloco(NoAST *bloco, NoAST *comando);
+
+
+void inicializarNo(NoAST *no);
+
+
 void imprimirAST(NoAST *no);
 int tiposCompativeis(Tipo t1, Tipo t2);
+void executarComando(NoAST *no);
 NoAST* interpretar(NoAST *no);
 void liberarAST(NoAST *no);
 void imprimirValor(NoAST *no); // Declaração de imprimirValor
