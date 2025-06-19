14dc110e024682b95356b1188d5bfb87ac8008cc
Caiomesvie <caiomesvie@DellG15Caio.localdomain>
2025-06-19 20:23:02 -0300

    Adicionando mais testes e atualizando alguns pontos

--- src/tests/tests.sh
+++ src/tests/tests.sh
@@ -1,14 +1,16 @@
 #!/bin/bash
 
+# Remove arquivos gerados da compilação
 rm -r portugol portugol.tab.c lex.yy.c portugol.tab.h
 
+# Compila o analisador
 bison -d ../portugol.y
 flex ../portugol.l
 
-
 gcc -I.. -o portugol portugol.tab.c ../tabela.c ../ast.c lex.yy.c -lfl
 
-for file in tests_arquivos/*.txt; do
+# Executa todos os arquivos .txt dentro de subpastas de tests_arquivos
+find tests_arquivos -type f -name '*.txt' | while read file; do
     echo "=========================="
     echo "Arquivo: $file"
     echo "--------------------------"
